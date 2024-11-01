e37d04e73ef6313fce11b8fda3ef451eb396a0b3
dragon6810 <35243479+dragon6810@users.noreply.github.com>
2024-11-01 09:31:20 -0400

    fixed some issues with awol fonts time to test it by loading fonts in aegis

--- build tools/common/wad.c
+++ build tools/common/wad.c
@@ -4,10 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void WriteWad(char* path, image_t* images, int nimages)
+void WriteWad(char* path, image_t* images, int nimages, font_t* fonts, int nfonts)
 {
     int i, j, k, l, m;
     image_t *curimg;
+    font_t *curfont;
 
     FILE* ptr;
     
@@ -17,6 +18,7 @@ void WriteWad(char* path, image_t* images, int nimages)
     int *datasizes;
     short ncolors = 256;
     int offs;
+    int misc;
 
     ptr = fopen(path, "wb");
     if(!ptr)
@@ -64,8 +66,19 @@ void WriteWad(char* path, image_t* images, int nimages)
         }
     }
 
+    for(i=nimages, curfont=fonts; i<nfonts+nimages; i++, curfont++)
+    {
+        dataoffsets[i] = datasize;
+
+        datasizes[i] = 16 + 4 * 256 + 2 + 3 * 256;
+        datasizes[i] += curfont->w * curfont->h;
+
+        datasize += datasizes[i];
+    }
+
     fwrite(magic, 1, 4, ptr);
-    fwrite(&nimages, sizeof(int), 1, ptr);
+    misc = nimages + nfonts;
+    fwrite(&misc, sizeof(int), 1, ptr);
     datasize += 12;
     fwrite(&datasize, sizeof(int), 1, ptr);
     datasize -= 12;
@@ -128,6 +141,50 @@ void WriteWad(char* path, image_t* images, int nimages)
         }
     }
 
+    for(i=nimages, curfont=fonts; i<nfonts+nimages; i++, curfont++)
+    {
+        ncolors = 1;
+        fwrite(&curfont->w, sizeof(int), 1, ptr);
+        fwrite(&curfont->h, sizeof(int), 1, ptr);
+        fwrite(&ncolors, sizeof(int), 1, ptr);
+        fwrite(&curfont->h, sizeof(int), 1, ptr);
+
+        ncolors = 0;
+        for(j=0, k=0; j<256; j++)
+        {
+            fwrite(&k, sizeof(short), 1, ptr);
+
+            if(curfont->glyfs[j])
+            {
+                fwrite(&curfont->glyfs[j]->w, sizeof(short), 1, ptr);
+                k += curfont->glyfs[j]->w;
+            }
+            else
+                fwrite(&ncolors, sizeof(short), 1, ptr);
+        }
+
+        for(k=0; k<curfont->h; k++)
+        {
+            for(j=0; j<256; j++)
+            {
+                if(!curfont->glyfs[j])
+                    continue;
+
+                for(l=0; l<curfont->glyfs[j]->w; l++)
+                    fwrite(&curfont->glyfs[j]->data[k * curfont->glyfs[j]->w + l], 1, 1, ptr);
+            }
+        }
+
+        ncolors = 256;
+        fwrite(&ncolors, sizeof(short), 1, ptr);
+        for(j=0; j<256; j++)
+        {
+            fwrite(&curfont->palette[j].b, 1, 1, ptr);
+            fwrite(&curfont->palette[j].g, 1, 1, ptr);
+            fwrite(&curfont->palette[j].r, 1, 1, ptr);
+        }
+    }
+
     ncolors = 0;
     for(i=0, curimg=images; i<nimages; i++, curimg++)
     {
@@ -138,6 +195,19 @@ void WriteWad(char* path, image_t* images, int nimages)
         fwrite(&ncolors, 1, 1, ptr);
         fwrite(&ncolors, 2, 1, ptr);
         fwrite(curimg->name, 1, 16, ptr);
+    }
+
+    for(i=nimages, curfont=fonts; i<nfonts+nimages; i++, curfont++)
+    {
+        fwrite(dataoffsets + i, sizeof(int), 1, ptr);
+        fwrite(datasizes + i, sizeof(int), 1, ptr);
+        fwrite(datasizes + i, sizeof(int), 1, ptr);
+        ncolors = 0x46;
+        fwrite(&ncolors, 1, 1, ptr);
+        ncolors = 0;
+        fwrite(&ncolors, 1, 1, ptr);
+        fwrite(&ncolors, 2, 1, ptr);
+        fwrite(curfont->name, 1, 16, ptr);
     }
 
     fclose(ptr);
--- build tools/common/wad.h
+++ build tools/common/wad.h
@@ -7,4 +7,12 @@ typedef struct
 	bitmap_t* bm;
 } image_t;
 
-void WriteWad(char* path, image_t* images, int nimages);
+typedef struct
+{
+    char name[16];
+    bitmap_t *glyfs[256];
+	rgb8_t palette[256];
+	int w, h;
+} font_t;
+
+void WriteWad(char* path, image_t* images, int nimages, font_t* fonts, int nfonts);
