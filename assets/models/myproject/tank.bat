d6136e596e9fe2756877ad019afd5914583091bd
dragon6810 <35243479+dragon6810@users.noreply.github.com>
2025-07-21 20:45:35 -0400

    removed old now unused stuff

--- assets/models/myproject/tank.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-REM Compiling using the Wall Worm Batch File...
-"C:/Users/theto/Documents/Github/aegis/SDK/bin/studiomdl.exe" "C:/Users/theto/Documents/Github/aegis/External/models/myproject/tank.qc"
-@pause
