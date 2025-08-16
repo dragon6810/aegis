5dd21cd3d105562b52eefcfde3a6bc964ae2a926
dragon6810 <35243479+dragon6810@users.noreply.github.com>
2025-08-16 12:04:19 -0400

    opening window with sdl (i like it better than glfw so far)

--- engine/public/engine/cl_Client.h
+++ engine/public/engine/cl_Client.h
@@ -2,6 +2,8 @@
 
 #include <memory>
 
+#include <SDL3/SDL.h>
+
 #include <engine/cl_PlayerInput.h>
 
 namespace engine::cl
@@ -9,8 +11,15 @@ namespace engine::cl
     class Client
     {
     private:
+        SDL_Window *win = NULL;
+    private:
+        void MakeWindow(void);
+        void PollWindow(void);
+        void DestroyWindow(void);
         void Init(void);
     public:
+        bool lastframe = false;
+
         // games: set this to your own input class if you want to.
         std::unique_ptr<PlayerInput> pinput;
 
--- engine/src/cl_Client.cpp
+++ engine/src/cl_Client.cpp
@@ -4,6 +4,34 @@
 
 #include "cl_Input.h"
 
+void engine::cl::Client::MakeWindow(void)
+{
+    this->win = SDL_CreateWindow("aegis", 640, 360, 0);
+}
+
+void engine::cl::Client::PollWindow(void)
+{
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) 
+    {
+        switch(event.type)
+        {
+        case SDL_EVENT_QUIT:
+            this->lastframe = true;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void engine::cl::Client::DestroyWindow(void)
+{
+    SDL_DestroyWindow(this->win);
+    this->win = NULL;
+}
+
 void engine::cl::Client::Init(void)
 {
     InputInit();
@@ -17,12 +45,17 @@ void engine::cl::Client::Setup(void)
 
 int engine::cl::Client::Run(void)
 {
+    SDL_Event event;
+
     Init();
 
-    while(1)
+    this->MakeWindow();
+    while(!this->lastframe)
     {
+        this->PollWindow();
         Console::ExecTerm();
     }
+    this->DestroyWindow();
 
     return 0;
 }
